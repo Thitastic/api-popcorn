05553d8d820248427c45023ce4badbc4c11beaa8
Thitastic <thitastic@gmail.com>
2022-06-17 16:28:52 +0700

    test

--- server/api/sms.js
+++ server/api/sms.js
@@ -4,36 +4,36 @@ const express = require('express')
 const api = express.Router()
 
 api.post('/', (req, res)=>{
-    res.send(sendSms(req.body.message))
+    res.send(req.body.message)
 })
 
 api.get('/test', (req, res)=>{
     res.send("Hello")
 })
 
-function sendSms(message){
-    const vonage = new Vonage({
-        apiKey: "b65409d0",
-        apiSecret: "agK00JLrgCm2e6SN"
-        })
-        const from = "POPCORN"
-        const to = message.number
-        const text = message.text
+// function sendSms(message){
+//     const vonage = new Vonage({
+//         apiKey: "b65409d0",
+//         apiSecret: "agK00JLrgCm2e6SN"
+//         })
+//         const from = "POPCORN"
+//         const to = message.number
+//         const text = message.text
 
-        vonage.message.sendSms(from, to, text, (err, responseData) => {
-            if (err) {
-                console.log(err);
-            } else {
-                if(responseData.messages[0]['status'] === "0") {
-                    console.log("Message sent successfully.");
-                    return 200
-                } else {
-                    console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-                    return 404
-                }
-            }
-        })
-}
+//         vonage.message.sendSms(from, to, text, (err, responseData) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 if(responseData.messages[0]['status'] === "0") {
+//                     console.log("Message sent successfully.");
+//                     return 200
+//                 } else {
+//                     console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+//                     return 404
+//                 }
+//             }
+//         })
+// }
 
 
 
